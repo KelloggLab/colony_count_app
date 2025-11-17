26c8697affab5499e0a0829c578cef53dab22116
ekellogg <lizkellogg@gmail.com>
2025-11-17 15:34:34 -0700

    added argument parsing for train_all

--- train_all.py
+++ train_all.py
@@ -2,9 +2,40 @@ from pathlib import Path
 import pandas as pd
 from PIL import Image
 import colony_count
+import argparse
 
-image_paths = ["test1.jng", "test2.jpg"]
-csv_paths   = ["points1.csv", "points2.csv"]
+
+import argparse
+
+def parse_args():
+    parser = argparse.ArgumentParser(description="Train colony counter")
+
+    parser.add_argument(
+        "--images",
+        required=True,
+        type=str,
+        help="Path to a text file containing a list of image paths"
+    )
+
+    parser.add_argument(
+        "--picked_points",
+        required=True,
+        type=str,
+        help="Path to a text file containing a list of CSV files with picked points"
+    )
+
+    return parser.parse_args()
+
+
+def load_list(path):
+    with open(path, 'r') as f:
+        return [line.strip() for line in f if line.strip()]
+
+
+args = parse_args()
+
+image_paths = load_list(args.images)
+csv_paths   = load_list(args.picked_points)
 
 images = [Image.open(p) for p in image_paths]
 df_list = [pd.read_csv(p) for p in csv_paths]
